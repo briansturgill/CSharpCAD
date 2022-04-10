8e7d9a24876a925f387adc55ae610d1438099149
Brian Sturgill <c.brian.sturgill@gmail.com>
2022-04-09 18:05:04 -0600

    Thorough cleanup of circle and ellipse.

--- CSharpCAD/src/primitives/circle.cs
+++ CSharpCAD/src/primitives/circle.cs
@@ -3,18 +3,19 @@ namespace CSharpCAD;
 public static partial class CSCAD
 {
     /// <summary>Construct a circle in two dimensional space where all points are at the same distance from the center.</summary>
-    public static Geom2 Circle(Opts opts)
+    /// <param name="center" default="(0,0)">Center of circle.</param>
+    /// <param name="radius">Radius of circle.</param>
+    /// <param name="segments">Number of segments in a full rotation of circle.</param>
+    /// <param name="startAngle">Begining of the rotation of the circle (in radians).</param>
+    /// <param name="endAngle">End of the rotation of the circle (in radians).</param>
+    public static Geom2 Circle(double radius = 1, int segments = 32,
+        double startAngle = 0, double endAngle = (Math.PI * 2), Vec2? center = null)
     {
-        var radius = opts.GetDouble("radius", 1.0);
-        int segments = opts.GetInt("segments", 32);
-        var center = opts.GetVec2("center", (0.0, 0.0));
-        double startAngle = opts.GetDouble("startAngle", 0);
-        double endAngle = opts.GetDouble("endAngle", (Math.PI * 2));
-
-        if (radius <= 0) throw new ArgumentException("Radius must have a postive value.");
+        if (radius <= 0.0) throw new ArgumentException("Radius value must be postive.");
         if (segments < 3) throw new ArgumentException("Segments must be at least 3.");
+        var _center = center ?? new Vec2(0, 0);
 
-        return Ellipse(new Opts{{"radius", (radius, radius)}, {"segments", segments},
-          {"center", (center.x, center.y)}, {"startAngle", startAngle}, {"endAngle", endAngle}});
+        return Ellipse(radius: (radius, radius), segments: segments,
+          center: _center, startAngle: startAngle, endAngle: endAngle);
     }
 }
--- CSharpCAD/src/primitives/ellipse.cs
+++ CSharpCAD/src/primitives/ellipse.cs
@@ -6,21 +6,18 @@ public static partial class CSCAD
      * <summary>Construct an axis-aligned ellipse in two dimensional space.</summary>
      * <remarks>
      * @see https://en.wikipedia.org/wiki/Ellipse
-     * @param {Object} [options] - options for construction
-     * @param {Array} [options.center=[0,0]] - center of ellipse
-     * @param {Array} [options.radius=[1,1]] - radius of ellipse, along X and Y
-     * @param {Number} [options.startAngle=0] - start angle of ellipse, in radians
-     * @param {Number} [options.endAngle=(Math.PI * 2)] - end angle of ellipse, in radians
-     * @param {Number} [options.segments=32] - number of segments to create per full rotation
      * </remarks>
+     * <param name="center">Center of Ellipse. Default: [0,0].</param>
+     * <param name="radius">Radius of ellipse, along X and Y axes. Default:[1,1]</param>
+     * <param name="startAngle">Start angle of ellipse, in radians</param>
+     * <param name="endAngle">End angle of ellipse, in radians.</param>
+     * <param name="segments">Number of segments to create per full rotation.</param>
      */
-    public static Geom2 Ellipse(Opts opts)
+    public static Geom2 Ellipse(Vec2? radius = null, int segments = 32,
+        Vec2? center = null, double startAngle = 0, double endAngle = (Math.PI * 2))
     {
-        var radius = opts.GetVec2("radius", (1.0, 1.0));
-        int segments = opts.GetInt("segments", 32);
-        var center = opts.GetVec2("center", (0.0, 0.0));
-        double startAngle = opts.GetDouble("startAngle", 0);
-        double endAngle = opts.GetDouble("endAngle", (Math.PI * 2));
+        var _radius = radius ?? new Vec2(1.0, 1.0);
+        var _center = center ?? new Vec2(0.0, 0.0);
 
         if (startAngle < 0) throw new ArgumentException("startAngle must be positive");
         if (endAngle < 0) throw new ArgumentException("endAngle must be positive");
@@ -40,26 +37,26 @@ public static partial class CSCAD
             rotation = endAngle + ((Math.PI * 2) - startAngle);
         }
 
-        var minradius = Math.Min(radius.x, radius.y);
+        var minradius = Math.Min(_radius.x, _radius.y);
         var minangle = Math.Acos(((minradius * minradius) + (minradius * minradius) - (C.EPS * C.EPS)) /
                                   (2 * minradius * minradius));
         if (rotation < minangle) throw new ArgumentException("startAngle and endAngle do not define a significant rotation");
 
         segments = Floorish(segments * (rotation / (Math.PI * 2)));
 
-        var centerv = new Vec2(center.x, center.y);
+        var centerv = new Vec2(_center.x, _center.y);
         var step = rotation / (double)segments; // radians per segment
 
         var points = new List<Vec2>(segments);
-        segments = (rotation < Math.PI * 2) ? segments + 1 : segments;
+        segments = (LessThanish(rotation, Math.PI * 2)) ? segments + 1 : segments;
         for (var i = 0; i < segments; i++)
         {
             var angle = (step * i) + startAngle;
-            var point = new Vec2(radius.x * cos(angle), radius.y * sin(angle));
+            var point = new Vec2(_radius.x * Math.Cos(angle), _radius.y * Math.Sin(angle));
             point = centerv.Add(point);
             points.Add(point);
         }
-        if (rotation < Math.PI * 2) points.Add(centerv);
+        if (LessThanish(rotation, Math.PI * 2)) points.Add(centerv);
         return new Geom2(points);
     }
 }
