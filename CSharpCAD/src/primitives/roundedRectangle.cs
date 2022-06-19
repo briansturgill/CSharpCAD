eaa8ebb929dc8dbac9023ff2a11155ae0ece30d8
Brian Sturgill <c.brian.sturgill@gmail.com>
2022-06-19 08:44:08 -0600

    Reimplemented  roudedRectangle.

--- CSharpCAD/src/primitives/roundedRectangle.cs
+++ CSharpCAD/src/primitives/roundedRectangle.cs
@@ -17,7 +17,7 @@ public static partial class CSCAD
      * </example>
      * <group>2D Primitives</group>
      */
-    public static Geom2 RoundedRectangle(Vec2? size = null, double roundRadius = 0.2,
+    public static Geom2 RoundedRectangle2(Vec2? size = null, double roundRadius = 0.2,
         int segments = 32, Vec2? center = null)
     {
         var _size = size ?? new Vec2(2, 2);
@@ -63,5 +63,90 @@ public static partial class CSCAD
         points.AddRange(corner2Points);
         points.AddRange(corner3Points);
         return new Geom2(points);
+    }
+
+    /**
+     * <summary>Construct an axis-aligned rectangle in 2D space with rounded corners.</summary>
+     * <remarks>
+     * The default center point is selected such that the bottom left
+     * corner of the rectangle is (0,0). (The rectangle is entirely in the first quadrant.)
+     * </remarks>
+     * <param name="size" default="(2,2)">Dimension of rounded rectangle: width and length.</param>
+     * <param name="roundRadius">Round radius of corners.</param>
+     * <param name="segments">Number of segments to create per full rotation.</param>
+     * <param name="center" default="(size.X/2,size.Y/2)">Center of rounded rectangle.</param>
+     * <example>
+     * var myshape = RoundedRectangle(size: (10, 20), roundRadius: 2);
+     * </example>
+     * <group>2D Primitives</group>
+     */
+    public static Geom2 RoundedRectangle(Vec2? size = null, double roundRadius = 0.2,
+        int segments = 32, Vec2? center = null)
+    {
+        var _size = size ?? new Vec2(2, 2);
+        var ticks = (int)((segments / 4.0) + 1);
+        var radsPerTick = (Math.PI / 2) / ticks;
+        var rr = new Vec2[ticks * 4];
+        var rr_i = 0;
+        int i;
+
+        // Bottom Left
+        var angle = Math.PI; // 180 - 270
+        var xOff = roundRadius;
+        var yOff = roundRadius;
+        rr[rr_i++] = new Vec2(xOff - roundRadius, yOff);
+        for (i = 1; i < ticks - 1; i++)
+        {
+            angle += radsPerTick;
+            rr[rr_i++] = new Vec2(xOff + (roundRadius * Math.Cos(angle)), yOff + (roundRadius * Math.Sin(angle)));
+        }
+        rr[rr_i++] = new Vec2(xOff, yOff - roundRadius);
+
+        // Bottom Right
+        angle = (Math.PI * 3) / 2; // 270 - 360(0)
+        xOff = _size.X - roundRadius;
+        yOff = roundRadius;
+        rr[rr_i++] = new Vec2(xOff, yOff - roundRadius);
+        for (i = 1; i < ticks - 1; i++)
+        {
+            angle += radsPerTick;
+            rr[rr_i++] = new Vec2(xOff + (roundRadius * Math.Cos(angle)), yOff + (roundRadius * Math.Sin(angle)));
+        }
+        rr[rr_i++] = new Vec2(xOff + roundRadius, yOff);
+
+        // Top Right
+        angle = 0.0; // 0 - 90
+        xOff = _size.X - roundRadius;
+        yOff = _size.Y - roundRadius;
+        rr[rr_i++] = new Vec2(xOff + roundRadius, yOff);
+        for (i = 1; i < ticks - 1; i++)
+        {
+            angle += radsPerTick;
+            rr[rr_i++] = new Vec2(xOff + (roundRadius * Math.Cos(angle)), yOff + (roundRadius * Math.Sin(angle)));
+        }
+        rr[rr_i++] = new Vec2(xOff, yOff + roundRadius);
+
+        // Top Left
+        angle = Math.PI / 2; // 90 - 180
+        xOff = roundRadius;
+        yOff = _size.Y - roundRadius;
+        rr[rr_i++] = new Vec2(xOff, yOff + roundRadius);
+        for (i = 1; i < ticks - 1; i++)
+        {
+            angle += radsPerTick;
+            rr[rr_i++] = new Vec2(xOff + (roundRadius * Math.Cos(angle)), yOff + (roundRadius * Math.Sin(angle)));
+        }
+        rr[rr_i++] = new Vec2(xOff - roundRadius, yOff);
+
+        if (center is not null)
+        {
+            var _center = new Vec2(((Vec2)center).X - _size.X / 2, ((Vec2)center).Y - _size.Y / 2);
+            for (i = 0; i < rr.Length; i++)
+            {
+                rr[i] = _center.Add(rr[i]);
+            }
+        }
+
+        return new Geom2(rr);
     }
 }
--- CSharpCADTests/src/primitives/roundedRectangle.test.cs
+++ CSharpCADTests/src/primitives/roundedRectangle.test.cs
@@ -7,6 +7,8 @@ namespace CSharpCADTests;
 [TestFixture]
 public class RoundedRectangleTests
 {
+    static bool WriteTests = false;
+
     [SetUp]
     public void Setup()
     {
@@ -29,28 +31,8 @@ public class RoundedRectangleTests
         var geometry = RoundedRectangle(center: (4, 5), segments: 16);
         Assert.DoesNotThrow(() => geometry.Validate());
         var obs = geometry.ToPoints();
-        var exp = new Vec2[] {
-          new Vec2(5, 5.8),
-          new Vec2(4.984775906502257, 5.876536686473018),
-          new Vec2(4.941421356237309, 5.941421356237309),
-          new Vec2(4.876536686473018, 5.984775906502257),
-          new Vec2(4.8, 6),
-          new Vec2(3.2, 6),
-          new Vec2(3.1234633135269823, 5.984775906502257),
-          new Vec2(3.0585786437626905, 5.941421356237309),
-          new Vec2(3.015224093497743, 5.876536686473018),
-          new Vec2(3, 5.8),
-          new Vec2(3, 4.2),
-          new Vec2(3.015224093497743, 4.123463313526982),
-          new Vec2(3.0585786437626905, 4.058578643762691),
-          new Vec2(3.1234633135269823, 4.015224093497743),
-          new Vec2(3.2, 4),
-          new Vec2(4.8, 4),
-          new Vec2(4.876536686473018, 4.015224093497743),
-          new Vec2(4.941421356237309, 4.058578643762691),
-          new Vec2(4.984775906502257, 4.123463313526982),
-          new Vec2(5, 4.2)
-        };
+        if (WriteTests) TestData.Make("RoundRectOptsExp1", obs);
+        var exp = UnitTestData.RoundRectOptsExp1;
         Assert.AreEqual(obs.Length, 20);
         Assert.IsTrue(Helpers.CompareArrays(obs, exp));
 
@@ -58,28 +40,8 @@ public class RoundedRectangleTests
         geometry = RoundedRectangle(size: (10, 6), segments: 16, center: (0, 0));
         Assert.DoesNotThrow(() => geometry.Validate());
         obs = geometry.ToPoints();
-        exp = new Vec2[] {
-          new Vec2(5, 2.8),
-          new Vec2(4.984775906502257, 2.8765366864730177),
-          new Vec2(4.941421356237309, 2.9414213562373095),
-          new Vec2(4.876536686473018, 2.984775906502257),
-          new Vec2(4.8, 3),
-          new Vec2(-4.8, 3),
-          new Vec2(-4.876536686473018, 2.984775906502257),
-          new Vec2(-4.941421356237309, 2.9414213562373095),
-          new Vec2(-4.984775906502257, 2.8765366864730177),
-          new Vec2(-5, 2.8),
-          new Vec2(-5, -2.8),
-          new Vec2(-4.984775906502257, -2.8765366864730177),
-          new Vec2(-4.941421356237309, -2.9414213562373095),
-          new Vec2(-4.876536686473018, -2.984775906502257),
-          new Vec2(-4.8, -3),
-          new Vec2(4.8, -3),
-          new Vec2(4.876536686473018, -2.984775906502257),
-          new Vec2(4.941421356237309, -2.9414213562373095),
-          new Vec2(4.984775906502257, -2.8765366864730177),
-          new Vec2(5, -2.8)
-        };
+        if (WriteTests) TestData.Make("RoundRectOptsExp2", obs);
+        exp = UnitTestData.RoundRectOptsExp2;
         Assert.AreEqual(obs.Length, 20);
         Assert.IsTrue(Helpers.CompareArrays(obs, exp));
 
@@ -87,30 +49,10 @@ public class RoundedRectangleTests
         geometry = RoundedRectangle(size: (10, 6), roundRadius: 2, segments: 16, center: (0, 0));
         Assert.DoesNotThrow(() => geometry.Validate());
         obs = geometry.ToPoints();
-        exp = new Vec2[] {
-          new Vec2(5, 1),
-          new Vec2(4.847759065022574, 1.7653668647301797),
-          new Vec2(4.414213562373095, 2.414213562373095),
-          new Vec2(3.7653668647301797, 2.8477590650225735),
-          new Vec2(3, 3),
-          new Vec2(-3, 3),
-          new Vec2(-3.7653668647301792, 2.8477590650225735),
-          new Vec2(-4.414213562373095, 2.414213562373095),
-          new Vec2(-4.847759065022574, 1.7653668647301797),
-          new Vec2(-5, 1.0000000000000002),
-          new Vec2(-5, -0.9999999999999998),
-          new Vec2(-4.847759065022574, -1.7653668647301792),
-          new Vec2(-4.414213562373095, -2.414213562373095),
-          new Vec2(-3.76536686473018, -2.8477590650225735),
-          new Vec2(-3.0000000000000004, -3),
-          new Vec2(2.9999999999999996, -3),
-          new Vec2(3.7653668647301792, -2.8477590650225735),
-          new Vec2(4.414213562373095, -2.414213562373095),
-          new Vec2(4.847759065022574, -1.7653668647301801),
-          new Vec2(5, -1.0000000000000004)
-        };
+        if (WriteTests) TestData.Make("RoundRectOptsExp3", obs);
+        exp = UnitTestData.RoundRectOptsExp3;
         Assert.AreEqual(obs.Length, 20);
-        Assert.IsTrue(Helpers.CompareArrays(obs, exp));
+        Assert.IsTrue(Helpers.CompareArraysNEVec2(obs, exp));
 
         // test segments
         geometry = RoundedRectangle(size: (10, 6), roundRadius: 2, segments: 64, center: (0, 0));
