c7b0f4cc913ce21b594603244b281895db3f9c54
Brian Sturgill <c.brian.sturgill@gmail.com>
2022-07-19 15:41:43 -0600

    Corrected documentation.

--- CSharpCAD/src/primitives/square.cs
+++ CSharpCAD/src/primitives/square.cs
@@ -9,19 +9,18 @@ public static partial class CSCAD
      * corner of the square is (0,0). (The square is entirely in the first quadrant.)
      * </remarks>
      * <param  name="size">Dimension of square.</param>
-     * <param name="center" default="(0,0)">Center of square.</param>
+     * <param name="center" default="(size/2, size/2)">Center of square.</param>
      * <example>
      * var myshape = Square(size: 10);
      * </example>
      * <group>2D Primitives</group>
      */
-    public static Geom2 Square(double? size = null, Vec2? center = null)
+    public static Geom2 Square(double size = 2, Vec2? center = null)
     {
-        var _size = size ?? 2;
-        var _center = center ?? new Vec2(_size/2, _size/2);
+        var _center = center ?? new Vec2(size/2, size/2);
 
-        if (_size <= 0) throw new ArgumentException("Option size must be greater than zero.");
+        if (size <= 0) throw new ArgumentException("Option size must be greater than zero.");
 
-        return Rectangle(center: _center, size: new Vec2(_size, _size));
+        return Rectangle(center: _center, size: new Vec2(size, size));
     }
 }
