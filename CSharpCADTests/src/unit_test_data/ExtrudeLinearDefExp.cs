03ec016b89014a833107f56ef1115f900611825e
Brian Sturgill <c.brian.sturgill@gmail.com>
2022-07-20 21:06:07 -0600

    ExtrudeLinear change made some tests data invalid.

--- CSharpCADTests/src/unit_test_data/ExtrudeLinearDefExp.cs
+++ CSharpCADTests/src/unit_test_data/ExtrudeLinearDefExp.cs
@@ -6,39 +6,63 @@ public static partial class UnitTestData {
   public static List<List<Vec3>> ExtrudeLinearDefExp = new List<List<Vec3>> {
       new List<Vec3> {
         new Vec3(5, -5, 0),
-        new Vec3(-5, -5, 0),
-        new Vec3(-5, 5, 0),
-        new Vec3(5, 5, 0)
+        new Vec3(5, 5, 0),
+        new Vec3(5, 5, 1)
     }, 
       new List<Vec3> {
+        new Vec3(5, -5, 0),
         new Vec3(5, 5, 1),
-        new Vec3(-5, 5, 1),
-        new Vec3(-5, -5, 1),
         new Vec3(5, -5, 1)
     }, 
       new List<Vec3> {
         new Vec3(5, 5, 0),
         new Vec3(-5, 5, 0),
+        new Vec3(-5, 5, 1)
+    }, 
+      new List<Vec3> {
+        new Vec3(5, 5, 0),
         new Vec3(-5, 5, 1),
         new Vec3(5, 5, 1)
     }, 
       new List<Vec3> {
         new Vec3(-5, 5, 0),
         new Vec3(-5, -5, 0),
+        new Vec3(-5, -5, 1)
+    }, 
+      new List<Vec3> {
+        new Vec3(-5, 5, 0),
         new Vec3(-5, -5, 1),
         new Vec3(-5, 5, 1)
     }, 
       new List<Vec3> {
         new Vec3(-5, -5, 0),
         new Vec3(5, -5, 0),
+        new Vec3(5, -5, 1)
+    }, 
+      new List<Vec3> {
+        new Vec3(-5, -5, 0),
         new Vec3(5, -5, 1),
         new Vec3(-5, -5, 1)
     }, 
       new List<Vec3> {
-        new Vec3(5, -5, 0),
-        new Vec3(5, 5, 0),
+        new Vec3(-5, -5, 1),
+        new Vec3(5, -5, 1),
+        new Vec3(5, 5, 1)
+    }, 
+      new List<Vec3> {
         new Vec3(5, 5, 1),
-        new Vec3(5, -5, 1)
+        new Vec3(-5, 5, 1),
+        new Vec3(-5, -5, 1)
+    }, 
+      new List<Vec3> {
+        new Vec3(5, 5, 0),
+        new Vec3(5, -5, 0),
+        new Vec3(-5, -5, 0)
+    }, 
+      new List<Vec3> {
+        new Vec3(-5, -5, 0),
+        new Vec3(-5, 5, 0),
+        new Vec3(5, 5, 0)
     }
   };
 }
--- CSharpCADTests/src/unit_test_data/ExtrudeLinearNoTwistExp.cs
+++ CSharpCADTests/src/unit_test_data/ExtrudeLinearNoTwistExp.cs
@@ -6,39 +6,63 @@ public static partial class UnitTestData {
   public static List<List<Vec3>> ExtrudeLinearNoTwistExp = new List<List<Vec3>> {
       new List<Vec3> {
         new Vec3(5, -5, 0),
-        new Vec3(-5, -5, 0),
-        new Vec3(-5, 5, 0),
-        new Vec3(5, 5, 0)
+        new Vec3(5, 5, 0),
+        new Vec3(5, 5, 15)
     }, 
       new List<Vec3> {
+        new Vec3(5, -5, 0),
         new Vec3(5, 5, 15),
-        new Vec3(-5, 5, 15),
-        new Vec3(-5, -5, 15),
         new Vec3(5, -5, 15)
     }, 
       new List<Vec3> {
         new Vec3(5, 5, 0),
         new Vec3(-5, 5, 0),
+        new Vec3(-5, 5, 15)
+    }, 
+      new List<Vec3> {
+        new Vec3(5, 5, 0),
         new Vec3(-5, 5, 15),
         new Vec3(5, 5, 15)
     }, 
       new List<Vec3> {
         new Vec3(-5, 5, 0),
         new Vec3(-5, -5, 0),
+        new Vec3(-5, -5, 15)
+    }, 
+      new List<Vec3> {
+        new Vec3(-5, 5, 0),
         new Vec3(-5, -5, 15),
         new Vec3(-5, 5, 15)
     }, 
       new List<Vec3> {
         new Vec3(-5, -5, 0),
         new Vec3(5, -5, 0),
+        new Vec3(5, -5, 15)
+    }, 
+      new List<Vec3> {
+        new Vec3(-5, -5, 0),
         new Vec3(5, -5, 15),
         new Vec3(-5, -5, 15)
     }, 
       new List<Vec3> {
-        new Vec3(5, -5, 0),
-        new Vec3(5, 5, 0),
+        new Vec3(-5, -5, 15),
+        new Vec3(5, -5, 15),
+        new Vec3(5, 5, 15)
+    }, 
+      new List<Vec3> {
         new Vec3(5, 5, 15),
-        new Vec3(5, -5, 15)
+        new Vec3(-5, 5, 15),
+        new Vec3(-5, -5, 15)
+    }, 
+      new List<Vec3> {
+        new Vec3(5, 5, 0),
+        new Vec3(5, -5, 0),
+        new Vec3(-5, -5, 0)
+    }, 
+      new List<Vec3> {
+        new Vec3(-5, -5, 0),
+        new Vec3(-5, 5, 0),
+        new Vec3(5, 5, 0)
     }
   };
 }
