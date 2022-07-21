9496ba5d5de610c7be2a2b4be3e3d535c93bd4b9
Brian Sturgill <c.brian.sturgill@gmail.com>
2022-07-20 20:31:47 -0600

    Stopped calling ExtrudeSimple from ExtrudeLinear, was not working in the case of stars.

--- CSharpCAD/src/operations/extrusions/extrudeLinear.cs
+++ CSharpCAD/src/operations/extrusions/extrudeLinear.cs
@@ -13,7 +13,9 @@ public static partial class CSCAD
      */
     public static Geom3 ExtrudeLinear(Geom2 gobj, double height = 1, double twistAngle = 0, int twistSteps = 1, bool repair = true)
     {
-        if (twistAngle == 0 && gobj.HasOnlyOnePath) return ExtrudeSimple(gobj, height);
+        // This test was not sufficient, ExtrudeSimple also requires it be convex.
+        // The need for this will go a way when all 3D are required to be triangulated.
+        //if (twistAngle == 0 && gobj.HasOnlyOnePath) return ExtrudeSimple(gobj, height);
         twistAngle = DegToRad(twistAngle);
         return ExtrudeLinearGeom2(gobj, new Vec3(0, 0, height), twistAngle, twistSteps, repair);
     }
