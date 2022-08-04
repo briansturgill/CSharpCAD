73fd4c7f05e0c40628f71bf014a5ba3b8f25edac
Brian Sturgill <c.brian.sturgill@gmail.com>
2022-08-04 17:57:11 -0600

    Remove polybool.net

--- CSharpCAD/src/operations/booleans/polybool.net/src/Epsilon.cs
+++
@@ -1,7 +0,0 @@
-﻿#nullable disable
-namespace CSharpCAD.PolyboolDotNet;
-
-internal static class Epsilon
-{
-    internal static double Eps { get; set; } = C.EPS;
-}
--- CSharpCAD/src/operations/booleans/polybool.net/src/Region.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
-#nullable disable
-namespace CSharpCAD.PolyboolDotNet;
-
-internal class Region
-{
-    internal List<Vec2> Points { get; set; }
-}
-*/
